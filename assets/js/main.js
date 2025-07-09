5e5b922bd0ca0a98652029e1a8a2535735352c42
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-07-09 16:26:46 +0300

    03. usefull-things-115-card-reveal. Bg, animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const drawer = document.querySelector(".drawer");
     const close = document.querySelector(".close");
     const cardsContainer = document.querySelector(".cards");
+
+    gsap.from(".nav .nav-item", 1, {
+        scale: 0,
+        opacity: 0,
+        stagger: {
+            amount: 0.3,
+        },
+        ease: "power4.inOut",
+    });
 
     const tl = gsap.timeline({
         paused: true,
@@ -12,21 +20,39 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         onReverseComplete: () => {
             cardsContainer.style.pointerEvents = "none";
-        }
+        },
     });
 
-    tl.from(".cards .card", 1.5, {
-        y: 1000,
-        stagger: {
-            amount: 0.3
-        },
-        ease: "power4.inOut"
-    }).from(".close", 0.5, {
-        scale: 0,
-        delay: 1
-    }, "<").from(".footer", 0.5, {
-        opacity: 0
-    });
+    tl.to(".cards", 1, {
+        background: "rgba(0,0,0,0.9)",
+        ease: "power4.inOut",
+    })
+        .from(".cards .card", 0.1, {
+            y: 1000,
+            stagger: {
+                amount: 0.3,
+            },
+            ease: "power4.inOut",
+        })
+        .to(".cards .card", 0.5, {
+            opacity: 1,
+            stagger: {
+                amount: 0.3,
+            },
+            ease: "power4.inOut",
+        })
+        .from(
+            ".close",
+            0.5,
+            {
+                scale: 0,
+                delay: 1,
+            },
+            "<"
+        )
+        .from(".footer", 0.5, {
+            opacity: 0,
+        });
 
     drawer.addEventListener("click", () => {
         if (tl.reversed()) {
@@ -39,24 +65,4 @@ document.addEventListener("DOMContentLoaded", () => {
     close.addEventListener("click", () => {
         tl.reverse();
     });
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
